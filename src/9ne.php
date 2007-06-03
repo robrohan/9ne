4d897c6e4a52b110a41a529ea97a26b9bd6d369c
Rob Rohan <me@robrohan.com>
2007-06-02 23:39:14 -0700

    Updating php to match the html version, also corrected spelling error with "themes"

--- src/9ne.php
+++ src/9ne.php
@@ -7,7 +7,7 @@
 	<meta name="author" content="Rob Rohan">
 	
 	<!-- just include all of Sortie -->
-	<script type="text/javascript" src="http://thoth.robrohan.com/projects/sortie/Sortie_0.6.6.js" encoding="utf-8"></script>
+	<script type="text/javascript" src="libs/Sortie_0.6.6.js" encoding="utf-8"></script>
 	<script type="text/javascript">
 		Sortie.Core.$({
 			include:new Array(
@@ -19,57 +19,19 @@
 						print('"bindings/WindowsBindings.js",');
 					}
 				?>
-				
-				//"modes/css/Mode.js",
-				//"modes/javascript/Mode.js",
-				//"modes/release/Mode.js",
-				//"modes/text/Mode.js",
-				//"modes/xml/Mode.js",
-				
-				//"thirdparty/yahoo-min.js",
-				//"thirdparty/event-min.js",
-				//"thirdparty/animation-min.js",
-				//"thirdparty/container-min.js",
-				//"thirdparty/dom-min.js",
-				
-				"src/9neMain.js",
-				"src/Keys.js",
-				"src/Modes.js",
-				"src/Functions.js",
-				"src/Undo.js"
+				"9ne_@VERSION@.js"
 			)
 		});
 		Sortie.Core.Include();
 	</script>
 	
 	<script type="text/javascript">
-		INSTALL_LOCATION = "http://192.168.1.3/9ne";
+		INSTALL_LOCATION = "http://localhost";
 		MAIN_FILE = "9ne.php";
 	</script>
 	
 	<link rel="stylesheet" type="text/css" href="style/Editor.css" encoding="utf-8">
-	<link rel="stylesheet" type="text/css" href="style/theme/Default.css" encoding="utf-8">
-	
-	<?php
-		//$user_agent = (isset($_SERVER['HTTP_USER_AGENT'])) ? strtolower($_SERVER['HTTP_USER_AGENT']) : "";
-		//if(stristr($user_agent, "Mac")) {
-		//	print('<script type="text/javascript" src="bindings/MacBindings.js" encoding="utf-8"></script>');
-		//} else {
-		//	print('<script type="text/javascript" src="bindings/WindowsBindings.js" encoding="utf-8"></script>');
-		//}
-	?>
-	<!-- <script type="text/javascript" src="src/Keys.js" encoding="utf-8"></script>
-	<script type="text/javascript" src="src/Modes.js" encoding="utf-8"></script>
-	<script type="text/javascript" src="src/Functions.js" encoding="utf-8"></script>
-	<script type="text/javascript" src="src/9neMain.js" encoding="utf-8"></script>
-	<script type="text/javascript" src="src/Undo.js" encoding="utf-8"></script> -->
-	
-	<!-- Supported Modes
-	<script type="text/javascript" src="modes/css/Mode.js" encoding="utf-8"></script>
-	<script type="text/javascript" src="modes/javascript/Mode.js" encoding="utf-8"></script>
-	<script type="text/javascript" src="modes/release/Mode.js" encoding="utf-8"></script>
-	<script type="text/javascript" src="modes/text/Mode.js" encoding="utf-8"></script>
-	<script type="text/javascript" src="modes/xml/Mode.js" encoding="utf-8"></script> -->
+	<link rel="stylesheet" type="text/css" href="style/themes/Default.css" encoding="utf-8">
 </head>
 <body onload="initEditor(); editor_focus();" onbeforeunload="return kill_9ne(); false; editor_focus();" onfocus="editor_focus();" onblur="editor_focus();">
 	<div id="editor" onmousedown="editor_focus()" onfocus="editor_focus();" onblur="editor_focus();">
